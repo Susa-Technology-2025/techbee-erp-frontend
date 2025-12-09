cb2e1fe92f149984d371f89141bb6adf0aa8264e
weletesadok <weletesadok@gmail.com>
2025-12-09 12:08:54 +0300

    use private cache implemented on main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,58 +5,69 @@ import "@fontsource/ubuntu/500.css";
 import "@fontsource/ubuntu/700.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { CustomThemeProvider } from "@/theme";
-import { ReactNode } from "react";
-// import { getThemeFromCookie } from "@/theme/theme-cookie";
+import { ReactNode, Suspense } from "react";
+import { getThemeFromCookie } from "@/theme/theme-cookie";
 import { Providers } from "@/lib/store/provider";
 import "./globals.css";
+import { headers } from "next/headers";
 
-// export async function generateMetadata() {
-//   const headerList = await headers();
-//   const host =
-//     headerList.get("x-forwarded-host") ||
-//     headerList.get("host") ||
-//     "erp.techbee.et";
-//   const subscribedTenants = ["minda", "vamdas"];
+export async function generateMetadata() {
+  "use cache: private";
+  const headerList = await headers();
+  const host =
+    headerList.get("x-forwarded-host") ||
+    headerList.get("host") ||
+    "erp.techbee.et";
+  const subscribedTenants = ["minda", "vamdas"];
 
-//   const tenant = subscribedTenants.includes(host.split(".")[0])
-//     ? host.split(".")[0]
-//     : "techbee";
+  const tenant = subscribedTenants.includes(host.split(".")[0])
+    ? host.split(".")[0]
+    : "techbee";
 
-//   const baseUrl = `https://${tenant}.techbee.et`;
+  const baseUrl = `https://${tenant}.techbee.et`;
 
-//   return {
-//     metadataBase: new URL(baseUrl),
-//     title: `${tenant.toUpperCase()} | ERP`,
-//     description: `AI-powered ERP platform customized for ${tenant}, enabling secure digital transformation and operational efficiency.`,
-//     keywords: [
-//       `${tenant} ERP`,
-//       "TechBee ERP",
-//       "ERP Ethiopia",
-//       "AI ERP",
-//       "Government Compliant ERP",
-//       "Digital Transformation",
-//       "Operational Efficiency",
-//     ],
-//     alternates: { canonical: baseUrl },
-//     openGraph: {
-//       title: `${tenant.toUpperCase()} | AI-Integrated ERP for Ethiopia`,
-//       description: `AI-Integrated, compliant ERP powering scalable digital transformation for ${tenant}.`,
-//       url: baseUrl,
-//       type: "website",
-//       images: [{ url: "/images/TechBee.png", width: 1200, height: 630 }],
-//     },
-//     twitter: {
-//       card: "summary_large_image",
-//       title: `${tenant.toUpperCase()} | TechBee ERP`,
-//       description: `AI-Integrated ERP platform empowering ${tenant}.`,
-//       images: ["/images/TechBee.png"],
-//     },
-//   };
-// }
+  return {
+    metadataBase: new URL(baseUrl),
+    title: `${tenant.toUpperCase()} | ERP`,
+    description: `AI-powered ERP platform customized for ${tenant}, enabling secure digital transformation and operational efficiency.`,
+    keywords: [
+      `${tenant} ERP`,
+      "TechBee ERP",
+      "ERP Ethiopia",
+      "AI ERP",
+      "Government Compliant ERP",
+      "Digital Transformation",
+      "Operational Efficiency",
+    ],
+    alternates: { canonical: baseUrl },
+    openGraph: {
+      title: `${tenant.toUpperCase()} | AI-Integrated ERP for Ethiopia`,
+      description: `AI-Integrated, compliant ERP powering scalable digital transformation for ${tenant}.`,
+      url: baseUrl,
+      type: "website",
+      images: [{ url: "/images/TechBee.png", width: 1200, height: 630 }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${tenant.toUpperCase()} | TechBee ERP`,
+      description: `AI-Integrated ERP platform empowering ${tenant}.`,
+      images: ["/images/TechBee.png"],
+    },
+  };
+}
 
-export default function Page({ children }: { children: ReactNode }) {
-  // const themeName = await getThemeFromCookie();
+async function ThemeProviderWrapper({ children }: { children: ReactNode }) {
+  "use cache: private";
+  const themeName = await getThemeFromCookie();
 
+  return (
+    <CustomThemeProvider initialTheme={themeName || "red"}>
+      {children}
+    </CustomThemeProvider>
+  );
+}
+
+export default async function Page({ children }: { children: ReactNode }) {
   return (
     <AppRouterCacheProvider options={{ enableCssLayer: true }}>
       <html lang="en">
@@ -69,9 +80,11 @@ export default function Page({ children }: { children: ReactNode }) {
             overflow: "hidden",
           }}
         >
-          <CustomThemeProvider initialTheme={"red"}>
-            <Providers>{children}</Providers>
-          </CustomThemeProvider>
+          <Suspense>
+            <ThemeProviderWrapper>
+              <Providers>{children}</Providers>
+            </ThemeProviderWrapper>
+          </Suspense>
         </body>
       </html>
     </AppRouterCacheProvider>
