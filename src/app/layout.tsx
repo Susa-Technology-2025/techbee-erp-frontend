ddc582d972c07a7e63bcd638ff6296f1b4758654
weletesadok <weletesadok@gmail.com>
2025-12-10 12:07:01 +0300

    session provider updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,53 +9,53 @@ import { ReactNode, Suspense } from "react";
 import { getThemeFromCookie } from "@/theme/theme-cookie";
 import { Providers } from "@/lib/store/provider";
 import "./globals.css";
-import { headers } from "next/headers";
-import { Toaster } from "react-hot-toast";
 
-export async function generateMetadata() {
-  "use cache: private";
-  const headerList = await headers();
-  const host =
-    headerList.get("x-forwarded-host") ||
-    headerList.get("host") ||
-    "erp.techbee.et";
-  const subscribedTenants = ["minda", "vamdas"];
+// import { headers } from "next/headers";
 
-  const tenant = subscribedTenants.includes(host.split(".")[0])
-    ? host.split(".")[0]
-    : "techbee";
+// export async function generateMetadata() {
+//   "use cache: private";
+//   const headerList = await headers();
+//   const host =
+//     headerList.get("x-forwarded-host") ||
+//     headerList.get("host") ||
+//     "erp.techbee.et";
+//   const subscribedTenants = ["minda", "vamdas"];
 
-  const baseUrl = `https://${tenant}.techbee.et`;
+//   const tenant = subscribedTenants.includes(host.split(".")[0])
+//     ? host.split(".")[0]
+//     : "techbee";
 
-  return {
-    metadataBase: new URL(baseUrl),
-    title: `${tenant.toUpperCase()} | ERP`,
-    description: `AI-powered ERP platform customized for ${tenant}, enabling secure digital transformation and operational efficiency.`,
-    keywords: [
-      `${tenant} ERP`,
-      "TechBee ERP",
-      "ERP Ethiopia",
-      "AI ERP",
-      "Government Compliant ERP",
-      "Digital Transformation",
-      "Operational Efficiency",
-    ],
-    alternates: { canonical: baseUrl },
-    openGraph: {
-      title: `${tenant.toUpperCase()} | AI-Integrated ERP for Ethiopia`,
-      description: `AI-Integrated, compliant ERP powering scalable digital transformation for ${tenant}.`,
-      url: baseUrl,
-      type: "website",
-      images: [{ url: "/images/TechBee.png", width: 1200, height: 630 }],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: `${tenant.toUpperCase()} | TechBee ERP`,
-      description: `AI-Integrated ERP platform empowering ${tenant}.`,
-      images: ["/images/TechBee.png"],
-    },
-  };
-}
+//   const baseUrl = `https://${tenant}.techbee.et`;
+
+//   return {
+//     metadataBase: new URL(baseUrl),
+//     title: `${tenant.toUpperCase()} | ERP`,
+//     description: `AI-powered ERP platform customized for ${tenant}, enabling secure digital transformation and operational efficiency.`,
+//     keywords: [
+//       `${tenant} ERP`,
+//       "TechBee ERP",
+//       "ERP Ethiopia",
+//       "AI ERP",
+//       "Government Compliant ERP",
+//       "Digital Transformation",
+//       "Operational Efficiency",
+//     ],
+//     alternates: { canonical: baseUrl },
+//     openGraph: {
+//       title: `${tenant.toUpperCase()} | AI-Integrated ERP for Ethiopia`,
+//       description: `AI-Integrated, compliant ERP powering scalable digital transformation for ${tenant}.`,
+//       url: baseUrl,
+//       type: "website",
+//       images: [{ url: "/images/TechBee.png", width: 1200, height: 630 }],
+//     },
+//     twitter: {
+//       card: "summary_large_image",
+//       title: `${tenant.toUpperCase()} | TechBee ERP`,
+//       description: `AI-Integrated ERP platform empowering ${tenant}.`,
+//       images: ["/images/TechBee.png"],
+//     },
+//   };
+// }
 
 async function ThemeProviderWrapper({ children }: { children: ReactNode }) {
   // "use cache: private";
@@ -81,7 +81,6 @@ export default async function Page({ children }: { children: ReactNode }) {
             overflow: "hidden",
           }}
         >
-          <Toaster />
           <Suspense>
             <ThemeProviderWrapper>
               <Providers>{children}</Providers>
