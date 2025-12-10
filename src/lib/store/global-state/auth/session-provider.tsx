92eeaf75529953baecb5e7956bc7e9fd8375023b
weletesadok <weletesadok@gmail.com>
2025-12-10 10:48:57 +0300

    session provider updated

--- src/lib/store/global-state/auth/session-provider.tsx
+++ src/lib/store/global-state/auth/session-provider.tsx
@@ -4,21 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { sessionActions } from "./auth-slice";
 import { useDataQuery } from "@/lib/tanstack/useDataQuery";
-import { usePathname } from "next/navigation";
-import { navItems } from "@/components/nav-items/nav-links-index";
 export default function SessionProvider({ children }: { children: ReactNode }) {
   const session = useSelector((state: RootState) => state.session);
-  const [enabled, setEnabled] = useState(false);
-  const pathname = usePathname();
-  useEffect(() => {
-    const itemology = navItems.map((item) => item.link);
-    itemology.push("/super-admin/landing-page");
-    itemology.push("/dashboard");
-    itemology.push("/test");
-    if (itemology.includes(pathname)) {
-      setEnabled(true);
-    }
-  }, [pathname]);
   const dispatch = useDispatch();
   useEffect(() => {
     const hostname = window.location.hostname;
@@ -33,7 +20,7 @@ export default function SessionProvider({ children }: { children: ReactNode }) {
     error,
   } = useDataQuery({
     apiEndPoint: "https://api.techbee.et/api/auth/auth/refresh-token",
-    enabled: Boolean(code) && !Boolean(session.tenantCode) && enabled,
+    enabled: Boolean(code) && !Boolean(session.tenantCode),
     fetchWithoutRefresh: true,
     tenantCode: code,
     noFilter: true,
