fb80fde96236ace0ee06c78299fb6f3b4a69a0f2
Ayele Masresha <weletesadok@gmail.com>
2025-12-11 10:05:05 +0300

    Add files via upload

---
+++ src/app/(authenticated)/project/[id]/layout.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+import SessioProvider from "@/lib/store/global-state/auth/session-provider";
+import CustomShapeBackground from "@/components/shapes/main-shape";
+import ResponsiveNavbar from "@/components/modulab/navbar";
+import { Box } from "@mui/material";
+import MiniSidebarDrawer from "@/components/modulab/sidebar";
+export default function Page({ children }: { children: ReactNode }) {
+  return (
+    <SessioProvider>
+      <Box
+        sx={{
+          display: "flex",
+          height: "100vh",
+          width: "100vw",
+          overflow: "hidden",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          bgcolor: "backgroundSection.main",
+        }}
+      >
+        <CustomShapeBackground />
+        <MiniSidebarDrawer />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            flex: 1,
+            overflow: "hidden",
+            p: 0,
+            m: 0,
+          }}
+        >
+          <ResponsiveNavbar />
+          <Box
+            sx={{
+              p: 2,
+              overflow: "auto",
+              position: "relative",
+            }}
+          >
+            {children}
+          </Box>
+        </Box>
+      </Box>
+    </SessioProvider>
+  );
+}
