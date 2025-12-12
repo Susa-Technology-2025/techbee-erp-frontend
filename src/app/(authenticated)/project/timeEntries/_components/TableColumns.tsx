a37781417dadf5cb14a1a8c6ccc89ac9f65cc6da
weletesadok <weletesadok@gmail.com>
2025-12-12 15:23:26 +0300

    schema changes

--- src/app/(authenticated)/project/timeEntries/_components/TableColumns.tsx
+++ src/app/(authenticated)/project/timeEntries/_components/TableColumns.tsx
@@ -1,10 +1,14 @@
 import { MRT_ColumnDef } from "material-react-table";
 export const columns: MRT_ColumnDef<any, any>[] = [
   { accessorKey: "type", header: "Type" },
-  { accessorKey: "billable", header: "Billable",
-  Cell: ({ cell }) => cell.getValue() ? "✔" : "✘" },
+  {
+    accessorKey: "billable",
+    header: "Billable",
+    Cell: ({ cell }) => (cell.getValue() ? "✔" : "✘"),
+  },
   { accessorKey: "userId", header: "Employee " },
   { accessorKey: "entryCode", header: "Entry Code" },
+  { accessorKey: "status", header: "Status" },
   { accessorKey: "taskOrActivity", header: "Task Or Activity" },
   { accessorKey: "notes", header: "Notes" },
   { accessorKey: "amount", header: "Amount" },
@@ -13,11 +17,14 @@ export const columns: MRT_ColumnDef<any, any>[] = [
   { accessorKey: "startTime", header: "Start Time" },
   { accessorKey: "endTime", header: "End Time" },
   { accessorKey: "attachmentUrl", header: "Attachment Url" },
-  { accessorKey: "workDate", header: "Work Date",
-  Cell: ({ cell }) => {
-    const v = cell.getValue();
-    return v ? new Date(v).toLocaleDateString() : "";
-  } },
+  {
+    accessorKey: "workDate",
+    header: "Work Date",
+    Cell: ({ cell }) => {
+      const v = cell.getValue();
+      return v ? new Date(v).toLocaleDateString() : "";
+    },
+  },
   { accessorKey: "timesheet.entryCode", header: "Timesheet Id" },
-  { accessorKey: "wbsItem.title", header: "Wbs Item Id" }
+  { accessorKey: "wbsItem.title", header: "Wbs Item Id" },
 ];
